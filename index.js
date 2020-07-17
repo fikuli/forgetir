ff66b1907a1285bde15b17db4bfc52be3c391cc8
Yilmaz <ext-bilge.yilmaz@here.com>
2020-07-17 19:12:56 +0200

    eslint

--- index.js
+++ index.js
@@ -13,20 +13,20 @@ app.post('/api/records', (request, response) => {
 		{
 			$project: {
 				_id: 0,
-				totalCount: { $sum: "$counts"},
-				createdAt: "$createdAt",
-				key: "$key"
+				totalCount: { $sum: '$counts'},
+				createdAt: '$createdAt',
+				key: '$key'
 			}
 		}, 
-		{ "$match" : { 
-			totalCount : { "$gte" : query.minCount, "$lte": query.maxCount },
-			createdAt: {"$gte" : new Date(query.startDate), "$lte": new Date(query.endDate)}
+		{ '$match' : { 
+			totalCount : { '$gte' : query.minCount, '$lte': query.maxCount },
+			createdAt: {'$gte' : new Date(query.startDate), '$lte': new Date(query.endDate)}
 		} }
 	]).then(records => {
 
 		let result ={
-			"code": 0,
-			"msg": "Success",
+			'code': 0,
+			'msg': 'Success',
 		}
 
 		result.records = records
