d3d98c9901752275c394ac95415c3b4283dec51b
Yilmaz <ext-bilge.yilmaz@here.com>
2020-07-17 18:44:34 +0200

    port

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ app.get('/api/records', (request, response) => {
 })
 ***/
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT||3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
