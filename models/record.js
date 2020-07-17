38ea972f6098b20d74273ce3db125829c20da9e4
Yilmaz <ext-bilge.yilmaz@here.com>
2020-07-17 18:30:25 +0200

    to hreoku

---
+++ models/record.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose')
+
+const url = process.env.MONGODB_URI
+
+console.log('connecting to', url)
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(result => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
+
+  const recordSchema = new mongoose.Schema({
+    key: String,
+    createdAt: Date,
+    counts: [Number],
+  })
+  
+  recordSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+      returnedObject.id = returnedObject._id.toString()
+      delete returnedObject._id
+      delete returnedObject.value
+    }
+  })
+  
+module.exports = mongoose.model('Record', recordSchema)
